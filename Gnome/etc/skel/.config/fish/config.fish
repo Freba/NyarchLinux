c8119f4ee1af5429de5ee8880195fb88e3afbd46
Freba <sricsi98@live.com>
2026-07-11 21:29:53 +0200

    pass arguments to function
    Currently there is no way to pass additional arguments to this custom
    function, so for example `history -R` won't do anything. Make it accept
    arguments to correc this behaviour.

--- Gnome/etc/skel/.config/fish/config.fish
+++ Gnome/etc/skel/.config/fish/config.fish
@@ -60,7 +60,7 @@ end
 
 # Fish command history
 function history
-    builtin history --show-time='%F %T '
+    builtin history --show-time='%F %T ' $argv
 end
 
 function backup --argument filename
